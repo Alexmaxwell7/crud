6dadc91a30d08c3a246d23b3c063457a87b0e969
Alexmaxwell7 <Alexmaxwell3096@gmail.com>
2021-04-21 10:56:30 +0530

    feat(controller component)

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors')
+const app= express();
 const url = 'mongodb://localhost/BookDB'
-
-const app = express()
 
 app.use(cors())
 
-mongoose.connect(url, {useNewUrlParser:true})
+mongoose.connect(url,{useNewUrlParser:true})
 const con = mongoose.connection
 
-con.on('open', () => {
-    console.log('connected...')
+con.on("open",()=>{
+    console.log("Server Connected")
 })
 
 app.use(express.json())
 
-const bookRouter = require('./routes/book')
-app.use('/book',bookRouter)
+app.use('/',require('./routes/book'))
 
-app.listen(5000, () => {
-    console.log('Server started')
+app.listen(5000,()=>{
+    console.log("server Started")
 })
+
+module.exports = app;
--- routes/book.js
+++ routes/book.js
@@ -1,67 +1,17 @@
-const express = require('express')
+const express = require('express');
+
+const Controller = require('../controller/book')
+
 const router = express.Router()
-const Books = require('../models/book')
-const to = require('await-to-js').default;
-const mongoose = require('mongoose')
-router.get('/', async(req,res) => {
 
-    let book,err;
-    [err,book]=await to (Books.find());
+router.get('/book', Controller.getBookDetails)
 
-    if(err){
-      return  res.status(500).json({'Error ' : err})
-    }
-    return res.status(200).json(book)
-  
-})
+router.get('/book/:id',Controller.getBookDetailsById)
 
-router.get('/:id', async(req,res) => {
-    let book,err;
-    [err,book]=await to (Books.findById(req.params.id));
-    if (err){
-        return res.status(500).json({'Error':err})
-    }
-    return res.status(200).json(book)
-})
+router.post('/book',Controller.InsertBookDetails)
 
+router.put('/book/:id',Controller.UpdateBookDetails)
 
-router.post('/', async(req,res) => {
-    const book = new Books({
-        title: req.body.title,
-        author: req.body.author,
-        rating:req.body.rating,
-        gender:req.body.gender,
-        publisher:req.body.publisher,
-        availability:req.body.availability,
-        // sub: req.body.sub
-    })
-    book.id=book._id;
-   const [err,books]=await to (book.save());
+router.delete('/book/:id',Controller.DeleteBookDetails)
 
-    if(err){
-        return res.send(500).json({'error':err})
-    }
-    return res.send(200).json(books) 
-})
-
-router.put('/:id',async(req,res)=> {
-    let book ;
-    [err,book]= await to(Books.findOneAndUpdate({_id:mongoose.Types.ObjectId(req.params.id)},req.body)); 
-    if(err){
-        return res.status(500).json({'Error':err})
-    }
-    return res.status(200).json(book)
-   
-})
-router.delete('/:id',async(req,res)=> {
-    let book ;
-    [err,book]= await to(Books.findOneAndDelete({_id:mongoose.Types.ObjectId(req.params.id)},req.body)); 
-    if(err){
-        return res.status(500).json({'Error':err})
-    }
-    return res.status(200).json(book)
-   
-})
-
-
-module.exports = router
+module.exports=router
